5bf6fae36cf5c8464098556d10b3f259c7d4e60b
piotr-yuxuan <pierre2b@adikteev.com>
2017-06-25 00:22:11 +0200

    Use lein-ancient to upgrate dependencies
    $ lein ancient upgrade :all :check-clojure :allow-all

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject mwe-cors-bug "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [reagent "0.6.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [reagent "0.6.2"]
                  [re-frame "0.9.4"]
                  [day8.re-frame/http-fx "0.1.3"]
-                 [cljs-ajax "0.5.8"]]
+                 [cljs-ajax "0.6.0"]]
 
-  :plugins [[lein-cljsbuild "1.1.4"]
+  :plugins [[lein-cljsbuild "1.1.6"]
             [lein-ancient "0.6.10"]]
 
   :min-lein-version "2.5.3"
@@ -19,9 +19,9 @@
 
   :profiles
   {:dev
-   {:dependencies [[binaryage/devtools "0.8.2"]]
+   {:dependencies [[binaryage/devtools "0.9.4"]]
 
-    :plugins      [[lein-figwheel "0.5.9"]]
+    :plugins      [[lein-figwheel "0.5.10"]]
     }}
 
   :cljsbuild
